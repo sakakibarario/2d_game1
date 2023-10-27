82b0ba6447e96cccd6839220f3176a908f2b00de
DEV2-33\GA1B <h1202319002@hiratagakuen.onmicrosoft.com>
2023-10-27 11:39:09 +0900

    時間制限完成

---
+++ Assets/邊見/TimeCounter.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeCounter : MonoBehaviour
+{
+    //カウントダウン
+    public float countdown = 10.0f;
+
+    //時間を表示するText型の変数
+    public Text timeText;
+
+    //ポーズしてるかどうか
+    private bool isPose = false;
+
+    //Uodate is called once per frame
+    void Update()
+    {
+        //クリックされたとき
+        if(Input.GetMouseButtonDown(0))
+        {
+            //ポーズ中にクリックされたとき
+            if(isPose)
+            {
+                //ポーズ状態を解除する
+                isPose = false;
+            }
+            //進行中にクリックされたとき
+            else{
+                //ポーズ状態にする
+                isPose = true;
+            }
+        }
+
+        //ポーズ中かどうか
+        if(isPose)
+        {
+            //ポーズ中であることを表示
+            timeText.text = "ポーズ中";
+
+            //カウントダウンしない
+            return;
+        }
+
+        //時間をカウントする
+        countdown -= Time.deltaTime;
+
+        //時間を表示する
+        timeText.text = countdown.ToString("f1") + "秒";
+
+        //countdownが0以下になったとき
+        if(countdown<=0)
+        {
+            timeText.text = "GAME OVER";
+        }
+    }
+}
