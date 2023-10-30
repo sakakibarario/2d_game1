892caf3a53fa1570ec2fe58b514bd3944d752969
森本瑞月 <h1202315026@hiratagakuen.onmicrosoft.com>
2023-10-30 10:28:07 +0900

    gameoverオブジェクト作成

--- Assets/森本/C#script/ToGameover.cs
+++ Assets/森本/C#script/ToGameover.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
 public class ToGameover : MonoBehaviour
 {
 
@@ -16,9 +17,19 @@ public class ToGameover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Return))
         {
             SceneManager.LoadScene("Gameover");
         }
     }
+    //Enemyタグと当たった時ゲームオーバー画面に移動
+    //private void OnTriggerEnter2D(Collider2D collision)
+    //{
+    //    if (collision.gameObject.tag=="Enemy")
+    //    {
+    //        SceneManager.LoadScene("Gameover");
+
+    //    }
+    //}
+
 }
