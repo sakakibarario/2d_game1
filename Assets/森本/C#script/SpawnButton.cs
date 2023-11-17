635feabbe8f8c7b89cde0b98d424bdfbb6a759be
森本瑞月 <h1202315026@hiratagakuen.onmicrosoft.com>
2023-11-17 10:16:10 +0900

    各座標ボタンを条件が満たされるまで出現しないようにした　条件はまだ記述していない

---
+++ Assets/森本/C#script/SpawnButton.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnButtonvillage : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        this.gameObject.SetActive(false);
+
+        //ƒ{ƒX‚ª“|‚³‚ê‚½‚ç‚ÌğŒ‚ğ‘‚­
+        //if()
+        //{
+        //    this.gameObject.SetActive(true);
+
+        //}
+    }
+}
