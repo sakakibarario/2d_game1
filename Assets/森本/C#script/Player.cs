7e3a5557e7df600322a0b22e1d2e8014829a3e00
森本瑞月 <h1202315026@hiratagakuen.onmicrosoft.com>
2023-11-22 13:52:51 +0900

    仮の主人公修正

--- Assets/森本/C#script/Player.cs
+++ Assets/森本/C#script/Player.cs
@@ -6,20 +6,16 @@ public class Player : MonoBehaviour
 {
     public float speed = 3f;
     private float playerSpeed;
-    public int hp;
 
-   // private float jumpForce = 350f;
-    //private int jumpCount = 0;
-
-    //Rigidbody2D rigidbody2D;
+    Rigidbody2D rigidbody2D;
 
     // Start is called before the first frame update
     void Start()
     {
-       // rigidbody2D = GetComponent<Rigidbody2D>();
+        rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
-    //Update is called once per frame
+    // Update is called once per frame
     void Update()
     {
         // ç∂ÉLÅ[ÇâüÇµÇΩÇÁç∂ï˚å¸Ç÷êiÇﬁ
@@ -29,19 +25,6 @@ public class Player : MonoBehaviour
         // âΩÇ‡Ç®Ç≥Ç»Ç©Ç¡ÇΩÇÁé~Ç‹ÇÈ
         else playerSpeed = 0;
 
-      //  rigidbody2D.velocity = new Vector2(playerSpeed, rigidbody2D.velocity.y);
-
-        //if (Input.GetMouseButtonDown(0) && this.jumpCount < 1)
-        //{
-        //    this.rbody2D.AddForce(transform.up * jumpForce);
-        //    jumpCount++;
-        //}
+        rigidbody2D.velocity = new Vector2(playerSpeed, rigidbody2D.velocity.y);
     }
-    //private void OnCollisionEnter2D(Collision2D other)
-    //{
-    //    if (other.gameObject.CompareTag("Floor"))
-    //    {
-    //        jumpCount = 0;
-    //    }
-    //}
 }
