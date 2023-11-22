3cdb7e10ae767563e899cbc7bc276f2d3b105056
森本瑞月 <h1202315026@hiratagakuen.onmicrosoft.com>
2023-11-22 10:06:29 +0900

    フリーズした

---
+++ Assets/森本/C#script/Hero.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hero : MonoBehaviour
+{
+    public GameObject Point1;
+    public GameObject Point2;
+    public GameObject Point3;
+    public GameObject thunder;
+    public GameObject Player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for(; Player.transform.position.x >0;)
+        {
+            int rnd = Random.Range(1, 4);
+
+            if(rnd == 1)
+            {
+                Point1.transform.position = thunder.transform.position;
+            }
+            if (rnd == 2)
+            {
+                Point2.transform.position = thunder.transform.position;
+            }
+            if (rnd == 3)
+            {
+                Point3.transform.position = thunder.transform.position;
+            }
+        }
+
+
+
+    }
+}
