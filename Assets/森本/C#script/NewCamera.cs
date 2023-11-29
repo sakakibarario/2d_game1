163137178acc8df224d5fcb20dcd7ae9bf154b5e
榊原吏雄 <h1202315009@hiratagakuen.onmicrosoft.com>
2023-11-29 12:15:23 +0900

    カメラの編集

--- Assets/森本/C#script/NewCamera.cs
+++ Assets/森本/C#script/NewCamera.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class NewCamera : MonoBehaviour
 {
     GameObject player;
+    //画面制御用
+    private int CPosleftx = -3;
+    private int PPosleftx = 7;
 
     // Start is called before the first frame update
     void Start()
@@ -15,9 +18,18 @@ public class NewCamera : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 playerPos = this.player.transform.position;
-        transform.position = new Vector3
-            (playerPos.x, playerPos.y +3.5f, transform.position.z);
+        Vector3 playerPos = this.player.transform.position;//playerのポジションを取得
+       
+        if(transform.position.x < CPosleftx)
+        {
+            transform.position = new Vector3
+           (transform.position.x, transform.position.y, transform.position.z);//画面を固定
+        }
+        else if(playerPos.x > PPosleftx)
+        {
+            transform.position = new Vector3
+           (playerPos.x, transform.position.y, transform.position.z);//playerに追従
+        }
 
         this.player = GameObject.Find("主人公");
     }
