3c05412c2ac0e14d15ad75c678a5fbc3aaa9cef7
森本瑞月 <h1202315026@hiratagakuen.onmicrosoft.com>
2023-11-29 15:26:16 +0900

    ボタンにSE追加

--- Assets/森本/C#script/Tomap.cs
+++ Assets/森本/C#script/Tomap.cs
@@ -8,13 +8,13 @@ public class Tomap : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //var fader = new FadeTransition()
     }
     public void OnClickStartButton()
     {
