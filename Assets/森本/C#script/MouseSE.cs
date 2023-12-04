71a6e44e680fda6f326dc415557315da1ae10e1f
森本瑞月 <h1202315026@hiratagakuen.onmicrosoft.com>
2023-12-04 10:07:55 +0900

    ボタンにマウスカーソルが重なったらSEがなるようにした　ボタンがないときは鳴らない

--- Assets/森本/C#script/MouseSE.cs
+++ Assets/森本/C#script/MouseSE.cs
@@ -10,16 +10,27 @@ public class MouseSE : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        MouseAudioSource = GetComponent<AudioSource>();
+        MouseAudioSource.mute = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (PlayerController.SougenBoss)
+        {
+            //村ボタン
+
+            MouseAudioSource.mute = !MouseAudioSource.mute;
+
+            if (PlayerController.VillageBoss)
+            {
+                //城ボタン
+                MouseAudioSource.mute = !MouseAudioSource.mute;
+            }
+
+        }
+
     }
-    private void OnMouseEnter()
-    {
-        MouseAudioSource.Play();
-    }
+
 }
