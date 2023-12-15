71f3017af89d7a12442a4f3e0f059cf9016eab37
森本瑞月 <h1202315026@hiratagakuen.onmicrosoft.com>
2023-12-15 11:20:31 +0900

    フリーズした

--- Assets/森本/C#script/ToEnd.cs
+++ Assets/森本/C#script/ToEnd.cs
@@ -7,24 +7,38 @@ public class ToEnd : MonoBehaviour
     public GameObject Endscene;
     public GameObject Button;
 
+    public byte END_count;
+    public byte END_counta;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Endscene.GetComponent<SpriteRenderer>().color = new Color32(255, 255, 255, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")//Player‚ª“–‚½‚Á‚½‚ç
         {
-            Endscene.gameObject.SetActive(true);
-            Button.gameObject.SetActive(true);
-        }
+            //Endscene.gameObject.SetActive(true);
 
+            for (END_count = 0; END_count < 255; END_count++)
+            {
+                if(END_count == 50)
+                {
+                    END_counta++;
+                    END_count = 0;
+                }
+                Endscene.GetComponent<SpriteRenderer>().color = new Color32(255, 255, 255, END_counta);
+            }
+            Button.gameObject.SetActive(true);
+
+        }
     }
 }
