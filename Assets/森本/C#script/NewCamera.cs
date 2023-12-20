06f7497a66feb6dd123981515579a7ffd88789f4
榊原吏雄 <h1202315009@hiratagakuen.onmicrosoft.com>
2023-12-20 09:54:53 +0900

    少し整理

--- Assets/森本/C#script/NewCamera.cs
+++ Assets/森本/C#script/NewCamera.cs
@@ -10,7 +10,7 @@ public class NewCamera : MonoBehaviour
     private int CPosleftx = -3;
     private float CPosright = 224.5f;
     private int PPosleftx = 7;
-    float i = 0.05f;
+    private float i = 0.05f;
 
     // Start is called before the first frame update
     void Start()
